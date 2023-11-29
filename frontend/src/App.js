271e7471f91bb38a95725fab1c8a7028f8714bbd
unknown <NahadAdmin@Cli3-034.Nahad.ir>
2023-11-29 11:04:07 +0330

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,10 @@ import Author from './Component/Autor';
 const App = () => {
   return (
     <div className="text-center flex flex-col items-center justify-center mb-8">
-      <Author faker={faker} />
-      <Author faker={faker} />
-      <Author faker={faker} />
+      <Author faker={faker} color={"bg-red-500"} />
+      <Author faker={faker} color={"bg-sky-500"} />
+      <Author faker={faker} color={"bg-teal-500"} />
+      <Author faker={faker} color={"bg-yellow-500"} />
     </div>
   );
 }
