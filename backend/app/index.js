3a5b3bd9bd6dcb9ff2c0b098fde389442f1e751b
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-30 11:42:11 +0330

    a

--- backend/app/index.js
+++ backend/app/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(cors({
     credentials: true,
 }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
+    next();
+})
 
 module.exports = (port) => {
     controllers(app, port)
