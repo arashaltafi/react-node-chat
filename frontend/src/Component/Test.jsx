39af1612d700711f931ff2d399c64b6cbd85edf0
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-06 21:23:12 +0330

    a

--- frontend/src/Component/Test.jsx
+++ frontend/src/Component/Test.jsx
@@ -1,10 +1,12 @@
-import { Axios } from 'axios'
+import Axios from 'axios'
 import React from 'react'
 import { useState, useEffect, useRef } from 'react'
 
 const Test = () => {
 
-    const [test, setTest] = useState(0)
+    const [catFact, setCatFact] = useState(0)
+    const [name, setName] = useState('')
+    const [age, setAge] = useState(0)
     useEffect(() => {
         console.log('call in created');
 
@@ -14,16 +16,38 @@ const Test = () => {
     }, [])
 
     useEffect(() => {
+        fetchCatFact()
+    }, [])
+
+    const fetchCatFact = () => {
         Axios.get('https://catfact.ninja/fact').then((res) => {
-            setTest(res.data.fact)
+            setCatFact(res.data.fact)
         }).catch((err) => {
             console.log(err.message);
         });
-    }, [])
+    }
+
+    const handleChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const fetchAgeGuess = () => {
+        Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+            setAge(res.data.age)
+        }).catch((err) => {
+            console.log(err.message);
+        });
+    }
 
     return (
         <div>
-            <h1>{test}</h1>
+            <h1>{catFact}</h1>
+            <button className='bg-red-500 p-2 m-2' onClick={fetchCatFact}>fetch cat fact</button>
+
+            <h1 className='mt-8'>guess age by name</h1>
+            <input className='p-2 text-lg text-center' placeholder='enter your name' type="text" onChange={handleChange} />
+            <button className='bg-red-500 p-2 m-2' onClick={fetchAgeGuess}>fetch cat fact</button>
+        <p className='mb-8'>{age > 0 && `your age is: ${age}`}</p>
         </div>
     )
 }
